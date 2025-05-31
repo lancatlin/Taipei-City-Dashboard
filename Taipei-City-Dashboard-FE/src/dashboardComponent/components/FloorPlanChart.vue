02f295c0e91b704df26415e858dfd913113b16ff
Caleb Rogers <caleb@508.dev>
2025-05-31 22:53:57 +0800

    Pad the house chart a bit

--- Taipei-City-Dashboard-FE/src/dashboardComponent/components/FloorPlanChart.vue
+++ Taipei-City-Dashboard-FE/src/dashboardComponent/components/FloorPlanChart.vue
@@ -258,7 +258,12 @@ function handleDataSelection(roomName) {
         </div>
       </div>
     <svg class="main-svg" viewBox="0 0 1501 1486" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <!-- Bounding Box -->
       <rect y="0" width="820.398" height="954" rx="32" fill="#111111" stroke="gray" />
+
+      <!-- Wrapper for all Other Elements -->
+      <g transform="translate(20,15)">
+
 
 <!-- Furniture -->
 <g>
@@ -469,7 +474,8 @@ function handleDataSelection(roomName) {
 </text>
 
 
-      </svg>
+      </g>
+    </svg>
     </div>
 
     <!-- Tooltip -->
